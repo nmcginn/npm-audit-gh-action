d6f26ac1d76808f6086ed65aa62d840980b3f423
Nathan McGinn <2986379+nmcginn@users.noreply.github.com>
2021-05-17 21:04:17 -0500

    check for vulnerabilities with npm audit

--- index.js
+++ index.js
@@ -1,27 +1,45 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+// const github = require('@actions/github');
 const { exec } = require("child_process");
 const fs = require('fs');
 
 try {
+  const failureLevel = core.getInput('fail-on');
   if (fs.existsSync('package.json')) {
     exec('npm audit --json', (error, stdout, stderr) => {
-      if (error) {
+      if (error && error.code !== 1) {
         console.error(stderr);
         throw error;
       }
       const v = JSON.parse(stdout);
-      console.log(`Vulnerabilities detected: ${v.metadata.vulnerabilities.critical} CRITICAL, ${v.metadata.vulnerabilities.high} HIGH, ${v.metadata.vulnerabilities.moderate} moderate, ${v.metadata.vulnerabilities.low} LOW`);
+      const criticalCount = v.metadata.vulnerabilities.critical;
+      const highCount = v.metadata.vulnerabilities.high;
+      const moderateCount = v.metadata.vulnerabilities.moderate;
+      const lowCount = v.metadata.vulnerabilities.low;
+      console.log(`Vulnerabilities detected: ${criticalCount} CRITICAL, ${highCount} HIGH, ${moderateCount} MODERATE, ${lowCount} LOW`);
+      switch (failureLevel) {
+        case 'critical':
+          if (criticalCount > 0)
+            core.setFailed(`Failing due to ${criticalCount} CRITICAL severity vulnerabilities. Please fix these with npm audit fix.`)
+          break;
+        case 'high':
+          if (highCount > 0 || criticalCount > 0)
+            core.setFailed(`Failing due to ${criticalCount+highCount} HIGH or worse severity vulnerabilities. Please fix these with npm audit fix.`)
+          break;
+        case 'moderate':
+          if (moderateCount > 0 || highCount > 0 || criticalCount > 0)
+            core.setFailed(`Failing due to ${criticalCount+highCount+moderateCount} MODERATE or worse severity vulnerabilities. Please fix these with npm audit fix.`)
+          break;
+        case 'low':
+          if (lowCount > 0 || moderateCount > 0 || highCount > 0 || criticalCount > 0)
+            core.setFailed(`Failing due to ${criticalCount+highCount+moderateCount+lowCount} LOW or worse severity vulnerabilities. Please fix these with npm audit fix.`)
+          break;
+        default:
+          console.log('Not failing the build, but you should address any detected vulnerabilities :)');
+          break;
+      }
     });
   }
-//   // `who-to-greet` input defined in action metadata file
-//   const nameToGreet = core.getInput('who-to-greet');
-//   console.log(`Hello ${nameToGreet}!`);
-//   const time = (new Date()).toTimeString();
-//   core.setOutput("time", time);
-//   // Get the JSON webhook payload for the event that triggered the workflow
-//   const payload = JSON.stringify(github.context.payload, undefined, 2)
-//   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
