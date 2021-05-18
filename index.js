c1af5cf91488cc954360a4173507902c6209f9c9
Nathan McGinn <2986379+nmcginn@users.noreply.github.com>
2021-05-17 20:47:14 -0500

    save

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const { exec } = require("child_process");
+const fs = require('fs');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  if (fs.existsSync('package.json')) {
+    exec('npm audit --json', (error, stdout, stderr) => {
+      if (error) {
+        console.error(stderr);
+        throw error;
+      }
+      const v = JSON.parse(stdout);
+      console.log(`Vulnerabilities detected: ${v.metadata.vulnerabilities.critical} CRITICAL, ${v.metadata.vulnerabilities.high} HIGH, ${v.metadata.vulnerabilities.moderate} moderate, ${v.metadata.vulnerabilities.low} LOW`);
+    });
+  }
+//   // `who-to-greet` input defined in action metadata file
+//   const nameToGreet = core.getInput('who-to-greet');
+//   console.log(`Hello ${nameToGreet}!`);
+//   const time = (new Date()).toTimeString();
+//   core.setOutput("time", time);
+//   // Get the JSON webhook payload for the event that triggered the workflow
+//   const payload = JSON.stringify(github.context.payload, undefined, 2)
+//   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
